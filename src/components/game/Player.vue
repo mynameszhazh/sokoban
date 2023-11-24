89e175b3f73c3487ad34ae7bc9a1b9e9403dfe00
mynameszhazh <1594776249@qq.com>
2023-11-24 11:10:56 +0800

    ci: 实现移动的基本盘

--- src/components/game/Player.vue
+++ src/components/game/Player.vue
@@ -9,17 +9,52 @@
 </template>
 
 <script setup lang="ts">
-import { reactive } from 'vue'
+import { onMounted, onUnmounted, reactive } from 'vue'
 import { ImgSource } from '@/composables/img'
 
-import { type Player, setupPlayer } from '@/game/player'
+import {
+  type Player,
+  setupPlayer,
+  moveUp,
+  moveDown,
+  moveLeft,
+  moveRight
+} from '@/game/player'
 import { usePosition } from '@/composables/position'
 
 const player = reactive({} as Player)
-
 setupPlayer(player)
-
 const { positionStyle } = usePosition(player)
+
+function useMove() {
+  function handleKeyup(e: KeyboardEvent) {
+    switch (e.code) {
+      case 'ArrowUp':
+        moveUp()
+        break
+      case 'ArrowDown':
+        moveDown()
+        break
+      case 'ArrowLeft':
+        moveLeft()
+        break
+      case 'ArrowRight':
+        moveRight()
+        break
+      default:
+        break
+    }
+  }
+
+  onMounted(() => {
+    window.addEventListener('keyup', handleKeyup)
+  })
+  onUnmounted(() => {
+    window.removeEventListener('keyup', handleKeyup)
+  })
+}
+
+useMove()
 </script>
 
 <style scoped></style>
--- src/game/player.ts
+++ src/game/player.ts
@@ -13,3 +13,19 @@ export function updatePlayer({ x, y }: { x: number; y: number }) {
   _player.x = x
   _player.y = y
 }
+
+export function moveUp() {
+  _player.y--
+}
+
+export function moveDown() {
+  _player.y++
+}
+
+export function moveLeft() {
+  _player.x--
+}
+
+export function moveRight() {
+  _player.x++
+}
